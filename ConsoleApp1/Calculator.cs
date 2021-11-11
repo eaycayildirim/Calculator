a7213eaf113bf371cd370c5acfaaeaaff9cd9d4a
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-11-11 21:27:38 +0100

    feat: Add Unit Test

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using nsIOperations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nsCalculator
+{
+    public class Calculator
+    {
+        public double Compute(double firstNumber, double secondNumber, IOperations operation)
+        {
+            return 5;
+        }
+    }
+}
---
+++ UnitTestCalculator/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using nsCalculator;
+using nsIOperations;
+
+namespace UnitTestCalculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SumTwoNumbers()
+        {
+            //Given
+            double firstNumber = 2;
+            double secondNumber = 3;
+            double expected = 5;
+            IOperations operation = new Sum();
+
+            //When
+            Calculator calculator = new Calculator();
+            double actual = calculator.Compute(firstNumber, secondNumber, operation);
+
+            //Then
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
