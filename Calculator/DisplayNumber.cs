d7c4ddf9a169b102746d3e5ab2df77771f432f56
Ayça Yıldırım <eaycayildirim@gmail.com>
2021-11-13 21:51:34 +0100

    feat: Add DisplayNumber and Tests

--- Calculator/DisplayNumber.cs
+++ Calculator/DisplayNumber.cs
@@ -6,15 +6,37 @@ namespace nsDisplayNumber
     {
         public DisplayNumber()
         {
-
+            Reset();
         }
 
-        public double ParseStringToDouble(string input)
+        public void Reset()
         {
-            return double.Parse(input);
+            DisplayNumberDouble = 0;
+            DisplayNumberString = "";
         }
 
+        public string Delete()
+        {
+            if(DisplayNumberString.Length > 0)
+                return DisplayNumberString.Remove(DisplayNumberString.Length-1);
+            else
+                return DisplayNumberString;
+        }
 
+        public string DecimalComma()
+        {
+            if(!IsDecimal())
+                return DisplayNumberString += ',';
+            else
+                return DisplayNumberString;
+        }
 
+        public double DisplayNumberDouble { get; set; }
+        public string DisplayNumberString { get; set; }
+
+        private bool IsDecimal()
+        {
+            return DisplayNumberString.Contains(",");
+        }
     }
 }
---
+++ UnitTestCalculator/UnitTestOperations/TestDisplayNumber.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nsDisplayNumber;
+
+namespace UnitTestCalculator
+{
+    [TestClass]
+    public class TestDisplayNumber : DisplayNumber
+    {
+        [TestMethod]
+        public void Delete_LastCharacter()
+        {
+            //Arrange
+            DisplayNumberString = "123";
+            var expected = "12";
+
+            //Act
+            var actual = Delete();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DecimalComma_NonDecimalNumber_CommaIsAdded()
+        {
+            //Arrange
+            DisplayNumberString = "123";
+            var expected = DisplayNumberString + ',';
+
+            //Act
+            var actual = DecimalComma();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DecimalComma_DecimalNumber_CommaIsNotAdded()
+        {
+            //Arrange
+            DisplayNumberString = "12,3";
+            var expected = DisplayNumberString;
+
+            //Act
+            var actual = DecimalComma();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
