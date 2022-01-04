1f08c2b7560ed68209cab70cb3cd0667f325eaed
Ayça Yıldırım <eaycayildirim@gmail.com>
2022-01-04 14:37:47 +0100

    feat: Added undefined class

---
+++ Calculator/Operations/Undefined.cs
@@ -0,0 +1,14 @@
+﻿namespace nsOperations
+{
+    public class Undefined : Operations
+    {
+        public Undefined() : base(new OperationData(false, false, ""))
+        {
+        }
+
+        public override double Compute(double firstNumber, double secondNumber)
+        {
+            return firstNumber;
+        }
+    }
+}
